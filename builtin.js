8294d8076cf3334126968a773a2458ed5f17ec75
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-07-30 06:28:56 +0530

    Update builtin.js

--- builtin.js
+++ builtin.js
@@ -42,6 +42,27 @@ arr.contains(value);
 arr.isEmpty();
 arr.indexOf(value);
 
+
+#########      Linked List       ###############
+
+Collections_framwork
+LinkedList <Integer> ll = new LinkedList<>();
+ll.addLast(value);
+ll.addFirst(value);
+ll.removeFirst(value);
+LinkedList <Integer> ll = new LinkedList<>();
+ll.removeLast(value);
+ll.get(index);
+ll.addAll(collect);
+ll.clear();
+ll.contains("value");
+ll.indexOf("value");
+
+
+LinkedList sec_ll = new LinkedList();
+sec_ll = (LinkedList) ll.clone();
+
+
 ###########   Hashing      ###########
 
 hm.put("key",value);
