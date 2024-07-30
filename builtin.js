7557381f1d2064c4472b2a0db67bbffd3fc2a00f
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-07-30 06:28:13 +0530

    Update builtin.js

--- builtin.js
+++ builtin.js
@@ -28,7 +28,19 @@ str.split("t", 0);
 
 str.equalsIgnoreCase(str1);
 
+##########     Array List   ###############
+List<String> myAlist = new ArrayList<String>();
+ArrayList<Integer> arr = new ArrayList<Integer>();
 
+
+myAlist.add("value");
+myAlist.remove("value");
+myAlist.get(index);
+arrlist1.addAll(arraylist2);
+arr.clear();
+arr.contains(value);
+arr.isEmpty();
+arr.indexOf(value);
 
 ###########   Hashing      ###########
 
