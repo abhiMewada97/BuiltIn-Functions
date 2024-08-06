51c6edd565f371cc691707c71e9eb3c6455f41c7
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-08-06 17:15:24 +0530

    LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+#########      Linked List       ###############
+
+Collections_framwork
+LinkedList <Integer> ll = new LinkedList<>();
+ll.addLast(value);
+ll.addFirst(value);
+ll.removeFirst(value);
+LinkedList <Integer> ll = new LinkedList<>();
+ll.removeLast(value);
+ll.get(index);
+ll.addAll(collect);
+ll.clear();
+ll.contains("value");
+ll.indexOf("value");
+
+
+LinkedList sec_ll = new LinkedList();
+sec_ll = (LinkedList) ll.clone();
+
