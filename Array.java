dffe111389058c615725f87afa8ff241be683101
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-08-06 17:14:20 +0530

    Array.java

---
+++ Array.java
@@ -0,0 +1,13 @@
+##########     Array List   ###############
+List<String> myAlist = new ArrayList<String>();
+ArrayList<Integer> arr = new ArrayList<Integer>();
+
+myAlist.set(index, value);
+myAlist.add("value");
+myAlist.remove("value");
+myAlist.get(index);
+arrlist1.addAll(arraylist2);
+arr.clear();
+arr.contains(value);
+arr.isEmpty();
+arr.indexOf(value);
