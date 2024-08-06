f522e083efaa2f5708c10eca72713ea86e918e42
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-08-06 17:16:27 +0530

    Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,7 @@
+#########    Conversion    #########
+
+int num = Integer.parseInt(args[i]);
+int num = Integer.valueOf(args[i]);
+Character.toLowerCase(s.charAt(i));
+char[] ch=str.toCharArray();              // string to array
+String str = String.join(" ",words);      //  array to string
