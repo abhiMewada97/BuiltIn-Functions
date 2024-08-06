aff8910b48d76b1122da948ba2e29fe26f4e5ebb
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-08-06 17:15:51 +0530

    Hashing.java

---
+++ Hashing.java
@@ -0,0 +1,15 @@
+###########   Hashing      ###########
+
+map.put("key",value);
+map.get("US");     // if not available return null
+map.remove("key");
+map.containsKey("key");     // if not available return false
+map.size();
+map.clear();
+
+LinkedHashSet <String> lhs = new LinkedHashSet<>();
+HashSet<Integer> set = new HashSet<>();
+TreeSet<Integer> set = new TreeSet<>();
+
+set.add(value);
+set.isEmpty();
