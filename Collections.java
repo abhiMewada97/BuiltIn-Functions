9822cc637dc1704a2c5282692e4621f0914d5216
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-09-28 10:02:53 +0530

    Create Collections.java

---
+++ Collections.java
@@ -0,0 +1,19 @@
+java.lang;
+interface Comparable<T> {
+  public int compareTo(T o);       // return  (+ve) , (-ve) , 0
+}
+
+class Song implements Comparable<Song> {
+  String title;
+  String artist;
+}
+
+class abc {
+  @Override
+  public int compareTo() {
+    return -----;
+  }
+
+  public static void main(String []args) {
+    
+}
