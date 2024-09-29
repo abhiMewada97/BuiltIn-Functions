b48e19901876cc15a133e0f7081d839c0125e577
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-09-29 10:30:10 +0530

    Update Collections.java

--- Collections.java
+++ Collections.java
@@ -1,3 +1,7 @@
+Comparator --> make our own change (own logic)
+Comparable --> use builtin
+
+
 java.lang;
 interface Comparable<T> {
   public int compareTo(T o);       // return  (+ve) , (-ve) , 0
