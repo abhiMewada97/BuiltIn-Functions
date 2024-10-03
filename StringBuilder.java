693dcc4849998f39b37264b0bceedbaac6afcaf9
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-10-03 16:15:12 +0530

    Update StringBuilder.java

--- StringBuilder.java
+++ StringBuilder.java
@@ -1,1 +1,18 @@
+StringBuilder sb = new StringBuilder();
+
+sb.append();
+sb.remove(index);
+sb.toString();
+sb.insert(1,"J")
+
+StringBuilder sb = new StringBuilder("Hello ");  
+sb.insert(1,"Java"); // HJavaello  
+
+StringBuilder sb = new StringBuilder("Hello");  
+sb.replace(1,3,"Java");  // HJavalo  
+
+StringBuilder sb=new StringBuilder("Hello");  
+sb.delete(1,3); // Hlo
+
+sb.reverse();  
 
