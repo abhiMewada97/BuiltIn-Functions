97530c18c74822b3443289562115f1c9b8a50c5f
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-10-04 10:52:44 +0530

    Update Conversion.java

--- Conversion.java
+++ Conversion.java
@@ -2,6 +2,12 @@
 
 int num = Integer.parseInt(args[i]);
 int num = Integer.valueOf(args[i]);
+
 Character.toLowerCase(s.charAt(i));
+
 char[] ch=str.toCharArray();              // string to array
 String str = String.join(" ",words);      //  array to string
+String str = new String(arr, start_Index, end_Index);      //  array to string
+
+String s = "asd ffgr rh h rwq wgr s";
+String[] arr = s.split(" ");
