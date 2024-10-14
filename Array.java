323237571ebf8b7aadbbf3e263731899bef56201
Abhishek Mewada <114232950+abhiMewada97@users.noreply.github.com>
2024-10-14 18:04:09 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -2,6 +2,9 @@
 List<String> myAlist = new ArrayList<String>();
 ArrayList<Integer> arr = new ArrayList<Integer>();
 
+// Convert the list to an array and return
+return resultList.toArray(new String[resultList.size()]);
+
 myAlist.set(index, value);
 myAlist.add("value");
 myAlist.remove("value");
